2f7bbb6b1802487555a9e035d1db5b7c0f713d72
brunoleitem <brunoleiteem@gmail.com>
2021-08-24 19:43:04 -0600

    api secret_key env

--- utils/request.js
+++ utils/request.js
@@ -1,10 +1,12 @@
+require('dotenv/config')
 const axios = require('axios').default;
+
 
 async function getRequest() {
 
     let response = await axios.get('http://api.portaldatransparencia.gov.br/api-de-dados/coronavirus/movimento-liquido-despesa?funcao=10&mesAnoLancamento=202105&pagina=1', 
     {
-        headers: { 'chave-api-dados': '8e131b6b1b354c1561c6f406e0a40112'}
+        headers: { 'chave-api-dados': process.env.SECRET_KEY}
     })
     
     const data = response.data;
